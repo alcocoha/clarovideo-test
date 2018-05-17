cf5855ca63bc6ce32d0da0c2a7ea328b47d864d7
Jorge Hurtado <jorhurta@mxmexpww6504.global.publicisgroupe.net>
2018-05-17 17:42:12 -0500

    ajustes de entrevista

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -38,8 +38,21 @@ class MovieList extends Component {
         this.initState(movies);
     }
 
+    quitaacentos = (s) => {
+    var r = s.toLowerCase();
+    r = r.replace(new RegExp(/\s/g), "");
+    r = r.replace(new RegExp(/[àáâãäå]/g), "a");
+    r = r.replace(new RegExp(/[èéêë]/g), "e");
+    r = r.replace(new RegExp(/[ìíîï]/g), "i");
+    r = r.replace(new RegExp(/ñ/g), "n");
+    r = r.replace(new RegExp(/[òóôõö]/g), "o");
+    r = r.replace(new RegExp(/[ùúûü]/g), "u");
+
+    return r;
+}
+
     onSearchChange = (query) => {
-        let searchMovies = this.state._movies.filter(data => data.title.toLowerCase().indexOf(query.toLowerCase()) !== -1);
+        let searchMovies = this.state._movies.filter(data => this.quitaacentos(data.rating_code).toLowerCase() == this.quitaacentos(query).toLowerCase());
         this.setState({
             ...this.state,
             movies: searchMovies
@@ -67,31 +80,40 @@ class MovieList extends Component {
     getMovie = () => {
         const { movies } = this.state;
 
-        return (movies.map(movie =>
-            <div className="col-6 col-md-3 img-container" key={movie.id}>
-                <p className="detail-container" data-tip={movie.title} data-for={movie.id}>
-                    <Link to={`/detail/${movie.id}`} title={movie.title} className="movie-link-detail">
-                        <div className="conteiner-icon">
-                            <i className="fa fa-play-circle-o play-icon"></i>
-                        </div>
-                        <img alt={movie.title} src={movie.image_small} className="img-movie d-none d-md-block" />
-                        <img alt={movie.title} src={movie.image_medium} className="img-movie-mobile d-md-none d-sm-block " />
-                    </Link>
-                </p>
-                <ReactTooltip id={movie.id} aria-haspopup='true' role='example' className="tool">
-                    <h1 className="tool-title">{movie.title}</h1>
-                    <p className="tool-duration">{movie.year}, duración: {movie.duration} hrs.</p>
-                    <p className="tool-decription">{movie.description}</p>
-                    <p className="tool-stars">
-                    {
-                        this.drawStars(movie.votes_average)
-                    }
+        console.log('movies');
+        console.log(movies.length);
+        if (movies.length > 0){
+            console.log("foo");
+            return (movies.map(movie =>
+                <div className="col-6 col-md-3 img-container" key={movie.id}>
+                    <p className="detail-container" data-tip={movie.title} data-for={movie.id}>
+                        <Link to={`/detail/${movie.id}`} title={movie.title} className="movie-link-detail">
+                            <div className="conteiner-icon">
+                                <i className="fa fa-play-circle-o play-icon"></i>
+                            </div>
+                            <img alt={movie.title} src={movie.image_small} className="img-movie d-none d-md-block" />
+                            <img alt={movie.title} src={movie.image_medium} className="img-movie-mobile d-md-none d-sm-block " />
+                            {movie.year}
+                        </Link>
                     </p>
-                </ReactTooltip>
-
-            </div>
-        )
-        );
+                    <ReactTooltip id={movie.id} aria-haspopup='true' role='example' className="tool">
+                        <h1 className="tool-title">{movie.title}</h1>
+                        <p className="tool-duration">{movie.year}, duración: {movie.duration} hrs.</p>
+                        <p className="tool-decription">{movie.description}</p>
+                        <p className="tool-stars">
+                        {
+                            this.drawStars(movie.votes_average)
+                        }
+                        </p>
+                    </ReactTooltip>
+    
+                </div>
+            )
+            );
+        } else {
+            console.log("bar");
+            return (<div style={{color:"yellow"}}>no hay</div>);
+        }
     }
 
     render() {
